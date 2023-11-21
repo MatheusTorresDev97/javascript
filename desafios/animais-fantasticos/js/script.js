89b87f008bd753167084039774006cfa83210b3e
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-21 08:25:01 -0300

    tooltip

--- desafios/animais-fantasticos/js/script.js
+++ desafios/animais-fantasticos/js/script.js
@@ -2,6 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
+import Tooltip from './modules/tooltip.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -14,3 +15,6 @@ tabNav.init();
 
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
+
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
