c05932f95352411cc28570bd976acccf7fd00f6a
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-21 08:16:07 -0300

    modal

--- desafios/animais-fantasticos/js/script.js
+++ desafios/animais-fantasticos/js/script.js
@@ -1,6 +1,7 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
+import Modal from './modules/modal.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -10,3 +11,6 @@ accordion.init();
 
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
+
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
